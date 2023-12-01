a47e6e1288da800dc1e01db13a30e7736c40cfae
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-01 15:41:05 -0500

    👌 IMPROVE: Refactor CartScreen component to use CartContext

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,16 +1,20 @@
 import ProductShopping from "@/components/ProductShopping"
-import {  ProductCart } from "@/interfaces/products"
+import { CartContext } from "@/context/cart/CartContext"
+import { useContext } from "react"
 
 export default function CartScreen  () {
-  const products: ProductCart[] = []
-  
+  const {addProduct,state, removeProduct} =useContext(CartContext)
+  const totalOrder = state.cart.reduce((acc, product) => acc + product.price, 0)
   return (
     <section>
+      <h1>Cart</h1>
       <ul>
-        {products.map((product) => (
-          <ProductShopping product={product} key={product.id} />
+        {state.cart.map((product) => (
+          <ProductShopping product={product} key={product.id} add={addProduct} remove={removeProduct}/>
         ))}
       </ul>
+      <h2>Total Order: {totalOrder}</h2>
+      <button>Checkout</button>
     </section>
   )
 }
