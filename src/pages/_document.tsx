38c86cfd810fa8df41daaf05bc74650fdb1a8c37
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-01 16:07:23 -0500

    📦 NEW: Add Footer and Header components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/ui/Footer"
+import Header from "@/components/ui/Header"
 import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
@@ -5,8 +7,10 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <Header />
         <Main />
         <NextScript />
+        <Footer />
       </body>
     </Html>
   )
