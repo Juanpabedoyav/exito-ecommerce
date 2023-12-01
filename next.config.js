3e94400f2a051570e203a4de99b89864cbb44bcd
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-01 13:13:05 -0500

    📦 NEW: Add scaffolding

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+const path = require("path")
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")]
+  }
 }
 
 module.exports = nextConfig
