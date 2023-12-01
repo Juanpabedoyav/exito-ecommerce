86519607f54c1f58b6efcede11bdde37ba47049b
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-01 18:53:52 -0500

    👌 IMPROVE: Update Header component and styles

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/Home.module.scss"
+import Image from "next/image"
 import Link from "next/link"
 
 
@@ -7,7 +8,7 @@ export default function Header  ()  {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <Link href={"/"}>
-          <h1>Next.js</h1>
+          <Image src={"https://exitocol.vtexassets.com/assets/vtex.file-manager-graphql/images/c10a9c1c-b69b-4955-9df8-5fa1854ad076___50dc4199e8020554de79a91123175966.svg"} alt="logo exito" width={80} height={80}/>
         </Link>
         <ul>
           <li>Categories</li>
