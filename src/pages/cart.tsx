ed44a3f52c1cb225f4d01c38b4e0a4f5fa8b0e44
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-02 17:51:48 -0500

    📦 NEW: Add Cart Empty state to Cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -4,7 +4,7 @@ import { CartContext } from "@/context/cart/CartContext"
 import Link from "next/link"
 import {  useContext, useEffect } from "react"
 import { useRouter } from "next/navigation"
-import { CreditCardIcon } from "@/components/Icons"
+import { CartEmptyIcon, CreditCardIcon } from "@/components/Icons"
 
 export default function Cart () {
   const router = useRouter()
@@ -19,6 +19,14 @@ export default function Cart () {
     <>
       <Link className={styles["link-redirect"]} href={"/"}>Back to Home</Link>
       <section className={styles["cart-container--full"]}>
+        {
+          state.cart.length === 0 && <div className={styles["cart-empty--full"]}>
+            <div>
+              {CartEmptyIcon()}
+            </div>
+            <h2>Cart Empty</h2>
+          </div>
+        }
         {
           state.cart.map((product) => (
             <ProductShopping product={product} key={product.id} add={addProduct} remove={removeProduct} removeOneByOne={removeProductByOne}/>
