2d247b1f2cb4f44922586c0579961da600e09313
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-03 13:32:49 -0500

    🐛 FIX: Remove unnecessary import statement in categories.test.tsx

--- __tests__/pages/categories.test.tsx
+++ __tests__/pages/categories.test.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import  Categories  from "@/pages/categories"
-import { describe } from "node:test"
 
 describe("Categories", () => {
   it("Should render list of categories", () => {
